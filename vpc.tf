47e4e612552ed8d7e92ebfe234ba6dfe896ffa40
unknown <satyanarayanaangadala4@gmail.com>
2026-03-04 16:15:57 +0530

    sj

--- vpc.tf
+++ vpc.tf
@@ -2,10 +2,6 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
     name="satya"
-<<<<<<< HEAD
-    ower="pod"
-=======
     ower="dev"
->>>>>>> 4b6af1cffde04446a4e98a6a5b1fcb3fcf61bda0
   }
 }
